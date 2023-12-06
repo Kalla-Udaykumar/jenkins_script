57b0dceac94bbfd9e88158c8bea4aca79766228b
Uday <102657313+Kalla-Udaykumar@users.noreply.github.com>
2023-12-06 07:40:54 +0530

    Update build.sh

--- Jira_37535/idf/build.sh
+++ Jira_37535/idf/build.sh
@@ -11,21 +11,13 @@ lts_kernel=${kernel_lts}
 echo ${rt_kernel}
 echo ${lts_kernel}
 
-dev_clone="git clone https://github.com/intel-innersource/drivers.ethernet.time-sensitive-networking.ese-linux-net-next.git -b dev/intel-mainline-tracking-6.5-rt5/master"
-rt_clone="git clone https://github.com/intel-innersource/os.linux.kernel.mainline-tracking-staging.git -b mainline-tracking-rt/v6.5-rt5"
+#dev_clone="git clone https://github.com/intel-innersource/drivers.ethernet.time-sensitive-networking.ese-linux-net-next.git -b dev/intel-mainline-tracking-6.5-rt5/master"
+#rt_clone="git clone https://github.com/intel-innersource/os.linux.kernel.mainline-tracking-staging.git -b mainline-tracking-rt/v6.5-rt5"
 get_config="wget http://oak-07.jf.intel.com/ikt_kernel_deb_repo/pool/main/l/linux-6.4-rt6-mainline-tracking-rt-230816t044845z/kernel.config"
-dev_fullpath="${dev_path}/drivers.ethernet.time-sensitive-networking.ese-linux-net-next"
-rt_fullpath="${rt_path}/os.linux.kernel.mainline-tracking-staging"
+#dev_fullpath="${dev_path}/drivers.ethernet.time-sensitive-networking.ese-linux-net-next"
+#rt_fullpath="${rt_path}/os.linux.kernel.mainline-tracking-staging"
 
 echo $PWD
-
-echo "========== git clone dev repo =========="
-cd ${dev_path}
-${dev_clone}
-
-echo "========== git clone IKT RT repo =========="
-cd ${rt_path}
-${rt_clone}
 
 echo "========== get RT config =========="
 cd ${rt_path}
@@ -36,28 +28,28 @@ i=0
 skip=0
 num=99
 
-cd ${rt_fullpath}
+cd ${rt_path}
 mkdir patches
 
 while [ "$i" -lt 20 ]
 do
-	cd ${dev_fullpath}
+	cd ${dev_path}
 	commit_title=$(git log -1 --skip $skip --pretty=format:"%s")
 	commit_title=${commit_title//\"/\\\"} #handle slash
 	commit_title=${commit_title//"*"/\\"*"} #handle asterisk
 
-	cd ${rt_fullpath}
+	cd ${rt_path}
 	search_result=$(git log --grep="${commit_title}" -1 --oneline)
 	
 	if [ -z "$search_result" ] 
 	then
 		echo "${commit_title}"
-		cd ${dev_fullpath}
+		cd ${dev_path}
 		commit_id=$(git log -1 --skip $skip --pretty=format:"%h")
 		format_title=${commit_title// /-}
 		format_title=${format_title////-}
 		format_title=${format_title//:/}
-		git format-patch -1 ${commit_id} --stdout > ${rt_fullpath}/patches/${num}-${format_title}.patch
+		git format-patch -1 ${commit_id} --stdout > ${rt_path}/patches/${num}-${format_title}.patch
 		num=`expr $num - 1`
 		i=0
 	else
@@ -71,12 +63,12 @@ if [ $num -eq 99 ]
 then
 	echo "No new patch!"
 else
-	cd ${rt_fullpath}
+	cd ${rt_path}
 	git am --whitespace=nowarn patches/*.patch
 fi
 
 echo "========== build RT kernel =========="
-cd ${rt_fullpath}
+cd ${rt_path}
 cp ../$(ls .. | grep .config) .config
 sed -i -e "s/CONFIG_LOCALVERSION=.*/CONFIG_LOCALVERSION=\"-ci-automation\"/" .config
 scripts/config --disable DEBUG_INFO

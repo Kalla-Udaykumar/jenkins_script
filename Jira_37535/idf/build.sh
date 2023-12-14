0ada5efff15f77f2c4642b3d67774ddddf9296f6
Uday <102657313+Kalla-Udaykumar@users.noreply.github.com>
2023-12-14 08:15:51 +0530

    Update build.sh

--- Jira_37535/idf/build.sh
+++ Jira_37535/idf/build.sh
@@ -70,8 +70,8 @@ fi
 echo "========== build RT kernel =========="
 cd ${rt_path}
 ls -la
-sleep 300
-cp ../$(ls .. | grep .config) .config
+# cp ../$(ls .. | grep .config) .config
+cp kernel.config .config
 sed -i -e "s/CONFIG_LOCALVERSION=.*/CONFIG_LOCALVERSION=\"-ci-automation\"/" .config
 scripts/config --disable DEBUG_INFO
 make olddefconfig && make -j12 bindeb-pkg

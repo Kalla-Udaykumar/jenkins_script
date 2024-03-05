cc1af9441c0ce71bac0e08a00aad024ece0d44e1
Uday <102657313+Kalla-Udaykumar@users.noreply.github.com>
2024-03-05 08:16:04 +0530

    Create build.sh

---
+++ jira-44015/idf/build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+export PATH=${PATH}:/usr/local/anaconda/bin && mkdir ${WORKSPACE}/upload && \
+cd ${WORKSPACE}/abi/mips && \
+export TENSORFLOW_CPU_LIB_PATH=/opt/intel/tf_lib && \
+export Torch_DIR=/opt/intel/ptlibs/libtorch/share/cmake/Torch && \
+export PYTORCH_CPU_LIB_PATH=/opt/intel/ptlibs/libtorch && \
+export TorchVision_DIR=/opt/intel/ptlibs/torchvision-install/share/cmake/TorchVision && \
+export TORCHVISION_CPU_LIB_PATH=/opt/intel/ptlibs/torchvision-install && \
+export OpenCV_INSTALL_DIR=/opt/intel/openvino_2022/opencv/build/install  && \
+export OpenCV_DIR=/opt/intel/openvino_2022/opencv/build/install/cmake && \
+export LD_LIBRARY_PATH=/opt/intel/openvino_2022/opencv/build/install/lib  && \
+export PYTHONPATH=/opt/intel/openvino_2022/opencv/build/install/python && \
+source /opt/intel/openvino_2022/setupvars.sh && \
+source /opt/intel/oneapi/setvars.sh && \
+mkdir build && \
+cd build && \
+cmake .. -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DBUILD_TEST=1 -DCMAKE_INSTALL_PREFIX=${WORKSPACE}/abi/mips-binary && \
+make -j\$(nproc) && \
+make install && \
+cp -r ${WORKSPACE}/abi/mips-binary/bin/mips_ut ${WORKSPACE}/upload/ && \
+cd ${WORKSPACE}/abi/ && \
+zip -r mips-binary.zip mips-binary && \
+ls -lrth && \
+cp mips-binary.zip ${WORKSPACE}/upload/ && \
+cd ${WORKSPACE}/abi/mips-binary/lib && \
+cp -r ${WORKSPACE}/mips-binary/lib/libmfdl.so libmfil.so libutils.so libjson-schema-validator.so.2.0.0 ${WORKSPACE}/upload/

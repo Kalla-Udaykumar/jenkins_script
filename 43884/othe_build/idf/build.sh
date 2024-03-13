fa0aa783b9e53ee6203e98be81e195ed4a8a33d2
Uday <102657313+Kalla-Udaykumar@users.noreply.github.com>
2024-03-13 14:19:59 +0530

    Update build.sh

--- 43884/othe_build/idf/build.sh
+++ 43884/othe_build/idf/build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -xe
  
 cd ${WORKSPACE}/abi/ubuntu_kernel
-wget http://oak-07.jf.intel.com/ikt_kernel_deb_repo/pool/main/l/linux-6.5-rt5-mainline-tracking-rt-231025t060334z/kernel.config
+wget http://oak-07.jf.intel.com/ikt_kernel_deb_repo/pool/main/l/lts-v6.6.15-rt22-preempt-rt-240227T180153Z/kernel.config
+#wget http://oak-07.jf.intel.com/ikt_kernel_deb_repo/pool/main/l/linux-6.5-rt5-mainline-tracking-rt-231025t060334z/kernel.config
 #cp -r ${WORKSPACE}/kernel.config ${WORKSPACE}/abi/ubuntu_kernel/
 mv kernel.config .config
 scripts/config --disable DEBUG_INFO

5b7ae2e41e160d50816b8e8fd5bcf72b5eb5b794
Uday <102657313+Kalla-Udaykumar@users.noreply.github.com>
2024-03-28 13:35:08 +0530

    Update cov_build.sh

--- jira-44015/idf/cov_build.sh
+++ jira-44015/idf/cov_build.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 #export PATH=${PATH}:/usr/local/anaconda/bin && mkdir ${WORKSPACE}/upload
-#cd ${WORKSPACE}/abi/mips
+cd ${WORKSPACE}/abi/mips
 #export TENSORFLOW_CPU_LIB_PATH=/opt/intel/tf_lib
 #export Torch_DIR=/opt/intel/ptlibs/libtorch/share/cmake/Torch
 #export PYTORCH_CPU_LIB_PATH=/opt/intel/ptlibs/libtorch
 #export TorchVision_DIR=/opt/intel/ptlibs/torchvision-install/share/cmake/TorchVision
 #export TORCHVISION_CPU_LIB_PATH=/opt/intel/ptlibs/torchvision-install
 #export OpenCV_INSTALL_DIR=/opt/intel/openvino_2022/opencv/build/install
-#export OpenCV_DIR=/opt/intel/openvino_2022/opencv/build/install/cmake
+export OpenCV_DIR=/opt/intel/openvino_2022/opencv/build/install/cmake
 #export LD_LIBRARY_PATH=/opt/intel/openvino_2022/opencv/build/install/lib
 #export PYTHONPATH=/opt/intel/openvino_2022/opencv/build/install/python
 source /opt/intel/openvino_2022/setupvars.sh
